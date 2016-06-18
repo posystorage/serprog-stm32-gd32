d1c73b4a5f8e8dd652a7675a22598f35b876c39e
posystorage <qq2545889167@gmail.com>
2016-06-18 20:32:15 +0800

    Revert "Revert "spi部分""
    This reverts commit 2e02e666e418d7b7780c311a35a34eac1c2a457c.

--- src/USB/CONFIG/hw_config.c
+++ src/USB/CONFIG/hw_config.c
@@ -20,6 +20,19 @@
 //Copyright(C) 广州市星翼电子科技有限公司 2009-2019
 //All rights reserved									  
 ////////////////////////////////////////////////////////////////////////////////// 
+
+/* Do not place const in front of declarations.                  *
+ * const variables are stored in flash that needs a 2-cycle wait */
+uint8_t  USB_Tx_Buf[VCP_DATA_SIZE];
+uint16_t USB_Tx_ptr_in  = 0;
+
+uint8_t  USB_Rx_Buf[VCP_DATA_SIZE];
+uint16_t USB_Rx_ptr_out = 0;
+uint8_t  USB_Rx_len     = 0;
+
+uint32_t val;
+
+
  
 _usb_usart_fifo uu_txfifo;					//USB串口发送FIFO结构体 
 u8  USART_PRINTF_Buffer[USB_USART_REC_LEN];	//usb_printf发送缓冲区
@@ -179,7 +192,7 @@ bool USART_Config(void)
 	printf("linecoding.bitrate:%d\r\n",linecoding.bitrate);
 	return (TRUE);
 }
- 
+ /*
 //处理从USB虚拟串口接收到的数据
 //databuffer:数据缓存区
 //Nb_bytes:接收到的字节数.
@@ -237,8 +250,80 @@ void usb_printf(char* fmt,...)
 	}
 } 
 
+*/
 
 
+void usb_putp(void) {
+  /* Previous transmission complete? */
+  while(GetEPTxStatus(ENDP1) != EP_TX_NAK);
+  /* Send buffer contents */
+  UserToPMABufferCopy(USB_Tx_Buf, ENDP1_TXADDR, USB_Tx_ptr_in);
+  SetEPTxCount(ENDP1, USB_Tx_ptr_in);
+  SetEPTxValid(ENDP1);
+  /* Reset buffer pointer */
+  USB_Tx_ptr_in = 0;
+}
+
+void usb_getp(void) {
+  /* Anything new? */
+  while(GetEPRxStatus(ENDP3) != EP_RX_NAK);
+  /* Get the length */
+  USB_Rx_len = GetEPRxCount(ENDP3);
+  if(USB_Rx_len > VCP_DATA_SIZE) USB_Rx_len = VCP_DATA_SIZE;
+  /* Fetch data and fill buffer */
+  PMAToUserBufferCopy(USB_Rx_Buf, ENDP3_RXADDR, VCP_DATA_SIZE);
+  /* We are good, next? */
+  SetEPRxValid(ENDP3);
+  USB_Rx_ptr_out = 0;
+}
+
+void usb_putc(char data) {
+  /* Feed new data */
+  USB_Tx_Buf[USB_Tx_ptr_in] = data;
+  USB_Tx_ptr_in ++;
+  /* End of the buffer, send packet now */
+  if(USB_Tx_ptr_in == VCP_DATA_SIZE) usb_putp();
+}
+
+char usb_getc(void) {
+  /* End of the buffer, wait for new packet */
+  if(USB_Rx_ptr_out == USB_Rx_len) usb_getp();
+  /* Get data from the packet */
+  USB_Rx_ptr_out ++;
+  return USB_Rx_Buf[USB_Rx_ptr_out - 1];
+}
+
+uint32_t usb_getu24(void) {
+  val = 0;
+
+  val  = (uint32_t)usb_getc() << 0;
+  val |= (uint32_t)usb_getc() << 8;
+  val |= (uint32_t)usb_getc() << 16;
+
+  return val;
+}
+
+uint32_t usb_getu32(void) {
+  val = 0;
+
+  val  = (uint32_t)usb_getc() << 0;
+  val |= (uint32_t)usb_getc() << 8;
+  val |= (uint32_t)usb_getc() << 16;
+  val |= (uint32_t)usb_getc() << 24;
+
+  return val;
+}
+
+void usb_putu32(uint32_t ww) {
+  usb_putc(ww >>  0 & 0x000000ff);
+  usb_putc(ww >>  8 & 0x000000ff);
+  usb_putc(ww >> 16 & 0x000000ff);
+  usb_putc(ww >> 24 & 0x000000ff);
+}
+
+void usb_sync(void) {
+  if(USB_Tx_ptr_in != 0) usb_putp();
+}
 
 
 
