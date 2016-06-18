8fba692c16f5e6d111e52243a736595dd78091ea
posystorage <qq2545889167@gmail.com>
2016-06-18 20:31:00 +0800

    Revert "create project"
    This reverts commit ecd9823fc16df609fd0e2e9d130c7b199d31c5c7.

--- example/io_spi.h
+++
@@ -1,8 +0,0 @@
-#ifndef __IO_SPI_H__
-#define __IO_SPI_H__
-
-extern uint32_t spi_conf(uint32_t speed_hz);
-extern void spi_bulk_write(uint32_t size);
-extern void spi_bulk_read(uint32_t size);
-
-#endif /* __IO_SPI_H__ */
